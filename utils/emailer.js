5c3ff40f2ebfab92f360636466787827bd7807aa
Abhijeet Singh Chauhan <abhijeetchauhan1005@gmail.com>
2025-04-24 00:01:48 +0530

    [ADD] implement ApiError, ApiResponse, asyncHandler, and emailer utilities

---
+++ utils/emailer.js
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config({
+    path: './.env'
+})
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GMAIL,
+        pass: process.env.GMAIL_PASS,
+    },
+});
+export const emailer = async (email, subject, htmlContent) => {
+    try {
+        const mailOptions = {
+            from: process.env.GMAIL,
+            to: email,
+            subject: subject,
+            html: htmlContent,
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Email Sent:", info.response);
+        return true;
+    } catch (error) {
+        console.error("Error sending email:", error);
+        return false;
+    }
+};
